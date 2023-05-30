a1338919a11c84eb49982676d4404a156bfce541
黄海 <hai.huang.a@outlook.com>
2023-05-31 00:42:13 +0800

    引入 dependencyManagement

--- order-center-job-common/src/test/java/com/nice/order/center/job/common/OrderCenterCommonApplicationTests.java
+++ order-center-job-common/src/test/java/com/nice/order/center/job/common/OrderCenterCommonApplicationTests.java
@@ -1,9 +1,7 @@
 package com.nice.order.center.job.common;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OrderCenterCommonApplicationTests {
 
     @Test
