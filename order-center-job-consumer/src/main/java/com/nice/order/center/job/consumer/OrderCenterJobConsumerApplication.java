7d82e4b0e4f13a1983bab91ddc782ad4a9629158
黄海 <hai.huang.a@outlook.com>
2023-07-27 23:50:53 +0800

    Nacos consumer

---
+++ order-center-job-consumer/src/main/java/com/nice/order/center/job/consumer/OrderCenterJobConsumerApplication.java
@@ -0,0 +1,24 @@
+package com.nice.order.center.job.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderCenterJobConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderCenterJobConsumerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
