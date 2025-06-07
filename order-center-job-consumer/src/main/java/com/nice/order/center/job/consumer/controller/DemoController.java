37328c2789d10490d1ac271e4738ecfb0fd862f0
黄海 <hh474440@cainiao.com>
2025-06-07 23:00:28 +0800

    Update

--- order-center-job-consumer/src/main/java/com/nice/order/center/job/consumer/controller/DemoController.java
+++ order-center-job-consumer/src/main/java/com/nice/order/center/job/consumer/controller/DemoController.java
@@ -1,16 +1,15 @@
 package com.nice.order.center.job.consumer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
-
 @RestController
 public class DemoController {
 
-    @Resource
+    @Autowired
     private RestTemplate restTemplate;
 
     /**
